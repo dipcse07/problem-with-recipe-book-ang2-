44c0edd60320a0af26b255e60c3ace91cc19adfa
dipcse07 <dipcse07@gmail.com>
2017-02-12 11:30:40 +0600

    clik at recipe-list.componet is fixed with click

--- recipes/recipe.ts
+++ recipes/recipe.ts
@@ -1,6 +1,6 @@
 export class Recipe {
 
-  constructor(public name, public description, public imagePath){
+  constructor(public name: string, public description: string, public imagePath: string ){
 
 
   }
